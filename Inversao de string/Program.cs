bf9bb9c25cbc470f24a85645ccad54e6e259c79c
Leonam Flores Mergareno <leonam.mergareno@sanesul.ms.gov.br>
2024-10-22 17:07:59 -0400

    Adicionar arquivos de projeto.

---
+++ Inversao de string/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inversao_de_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite a palavra a ser invertida\n");
+                string entrada = Console.ReadLine();
+
+                string invertida = "";
+
+                for (int i = entrada.Length - 1; i >= 0; i--)
+                {
+                    invertida += entrada[i];
+                }
+
+                Console.WriteLine("\nString original: " + entrada);
+                Console.WriteLine("String invertida: " + invertida);
+
+                Console.WriteLine("\n\nDigite 0 para fechar\n");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+            } while (valor != 0);
+        }
+    }
+}
---
+++ Inversao de string/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Inversao de string")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SANESUL")]
+[assembly: AssemblyProduct("Inversao de string")]
+[assembly: AssemblyCopyright("Copyright © SANESUL 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("080ad5b9-f2a4-4382-a22b-5e975afbf562")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
